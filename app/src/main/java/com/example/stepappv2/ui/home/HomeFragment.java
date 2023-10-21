beb8126abd7524f6451c09f1df6be8aeea532717
Isin Su Ecevit <ecevii@usi.ch>
2023-10-21 13:21:32 +0200

    Step 3-4: Use buttons to control progress

--- app/src/main/java/com/example/stepappv2/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/stepappv2/ui/home/HomeFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -14,18 +15,27 @@ import com.example.stepappv2.R;
 import com.example.stepappv2.databinding.FragmentHomeBinding;
 import com.google.android.material.progressindicator.CircularProgressIndicator;
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements View.OnClickListener {
 
     private FragmentHomeBinding binding;
     private TextView stepCountsView;
     private CircularProgressIndicator progressBar;
 
+    private int counter = 0;
+
+    @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         HomeViewModel homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        Button countButton = (Button) root.findViewById(R.id.count_button);
+        countButton.setOnClickListener(this);
+
+        Button startButton = root.findViewById(R.id.start_button);
+        startButton.setOnClickListener(this);
 
         stepCountsView = (TextView) root.findViewById(R.id.counter);
         stepCountsView.setText("1000");
@@ -35,6 +45,17 @@ public class HomeFragment extends Fragment {
         progressBar.setProgress(1000);
 
         return root;
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.count_button) {
+            counter++;
+        } else if (view.getId() == R.id.start_button) {
+            counter = 0;
+        }
+        stepCountsView.setText(Integer.toString(counter));
+        progressBar.setProgress(counter);
     }
 
     @Override
